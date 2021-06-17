17bc4b32e6c317e31c790ab0bc36ad01644612a2
kevin <kevin.madrigali@stud.unifi.it>
2021-06-17 16:19:15 +0200

    Added base scheme with methods/objects for the mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,69 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMainWindow>
+#include <QHeaderView>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow()
 {
-    ui->setupUi(this);
+
 }
 
+
+void MainWindow::createFilters(QHBoxLayout *layout)
+{
+
+}
+
+void MainWindow::initializeTable()
+{
+
+}
+
+void MainWindow::addTask()
+{
+
+}
+
+void MainWindow::newDatafile()
+{
+
+}
+
+void MainWindow::changeDB()
+{
+
+}
+
+void MainWindow::exitProgram()
+{
+    exit(0);
+}
+
+void MainWindow::cellSelected(int nRow, int nCol)
+{
+
+}
+
+void MainWindow::updateTable(vector<string*> data){
+
+}
+
+
+void MainWindow::filter()
+{
+
+}
+
+
+int MainWindow::getWeekNumber(tm t)
+{
+
+}
+
+MainWindow::~MainWindow()
+{
+
+}
 MainWindow::~MainWindow()
 {
     delete ui;
--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,64 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QRadioButton>
+#include <QCheckBox>
+#include <QTableWidget>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <sstream>
+#include <iomanip>
+#include <QFileDialog>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+using namespace std;
+
+namespace Ui { //semplificazione namespace
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow();
+    inline void setPath(string path){ this->path = path; }
     ~MainWindow();
+
+private slots:
+    void addTask();
+    void changeDB();
+    void exitProgram();
+    void newDatafile();
+    void cellSelected(int nRow, int nCol);
+
+public slots:
+    void filter();
 
 private:
     Ui::MainWindow *ui;
+    void createMenus();
+    void createFilters(QHBoxLayout *layout);
+    void initializeTable();
+    int getWeekNumber(tm t);
+    void updateTable(vector<string*> data);
+    string path;
+    QRadioButton *rb_all;
+    QRadioButton *rb_overdue;
+    QRadioButton *rb_today;
+    QRadioButton *rb_thisweek;
+    QCheckBox *cb_completed;
+    QTableWidget *table;
+    QStringList tableHeader;
+    QMenu *EditMenu;
+    QMenu *OptionsMenu;
+    QAction *newDatafileAct;
+    QAction *addTaskAct;
+    QAction *changeDBAct;
+    QAction *exitProgramAct;
 };
+
 #endif // MAINWINDOW_H
